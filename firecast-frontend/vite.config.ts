e68578551ec9a6bde29d31bfce6c7992afdd7eed
Donald Antonio Young <youngdo@oregonstate.edu>
2025-01-29 19:38:21 -0500

    shadcn init

--- firecast-frontend/vite.config.ts
+++ firecast-frontend/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import path from "path"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["src/__tests__/setup.ts"],
   },
+  
 });
